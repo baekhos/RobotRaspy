41fe829e2786e13c824daac4300165937294c95f
baekhos <george.nitrogen@gmail.com>
2018-05-27 21:07:05 +0000

    Add files via upload

--- motorp.py
+++ motorp.py
@@ -1,21 +1,26 @@
-import rospy
+#!/usr/bin/env python
+#motorp.py
 
+import rospy
+import curses
 from std_msgs.msg import String
 
-# screen = curses.initscr()
-# curses.noecho() 
-# curses.cbreak()
-# screen.keypad(True) 
+screen = curses.initscr()
+curses.noecho() 
+curses.cbreak()
+screen.keypad(True) 
 
 rospy.init_node('Motor_Publisher')	# Initializing the node
 pub = rospy.Publisher('direction', String, queue_size=10)
-
+print 'Enter W/A/S/D to controlthe robot: '
 while not rospy.is_shutdown():		# The function will return True if the node is ready to be shut down and False otherwise
     # This node publishes the input data on the topic "rotation"
     rate = rospy.Rate(1)
-    data = raw_input('Enter W/A/S/D to controlthe robot')
-    #data = screen.getch()
+    print 'Input:'
+    data = chr(screen.getch())
     data = data.upper()
-    if data in ("W","S","A","D" ):
+    print data
+   
+    if data in ("W","S","A","D","Q"):
         pub.publish(data)
         rate.sleep()
--- motors.py
+++ motors.py
@@ -1,23 +1,29 @@
+#!/usr/bin/env python
+#motorp.py
+
 import time
 import os
 import rospy
 from std_msgs.msg import String
 import RPi.GPIO as GPIO
+import sys
 #implementam si encoder ul tot aici
 #nr de gauri pe encoder este = 20
 #diametrul rotii este = 6.4 cm
 #pe 20 de roatatii avem 20 de cm
 #deci 1 cm pe gaura encoder
+#rezolutia encoder ului este de ~0.5 cm
+time.sleep(5)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
-GPIO.setup(8,GPIO.IN)
+GPIO.setup(40,GPIO.IN)
 
 def set(property, value):
     try:
-        f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+        f = open("/sys/class/pwm/" + property, 'w')
         f.write(value)
         f.close()   
     except:
@@ -56,30 +62,35 @@ def turnLeft():
 
 def EncoderMeasure():
     cm=0;
-    oldValue=newValue=False    
+    oldValue=newValue=GPIO.input(40)    
     end=start= time.time()
     while end - start < 0.5:
         end= time.time()
-        newValue = GPIO.input(8)
-        if newValue==True and oldValue==False :
-            cm+=1
-        oldValue=newValue
+        newValue = GPIO.input(40)
+        if newValue!=oldValue :
+            cm+=0.5
+        oldValue=newValue        
     GPIO.output(7,False)
     GPIO.output(11,False)
     GPIO.output(13,False)
     GPIO.output(15,False)
+    return cm
 
 def callback(msg):    
     if msg.data=="W":
         forward()
-    elif char == "S":
+    elif msg.data == "S":
         backwards()
-    elif char == "D":
+    elif msg.data == "D":
         turnRight()
-    elif char == "A":
+    elif msg.data == "A":
         turnLeft()
+    elif msg.data == "Q":
+        GPIO.cleanup()
+        sys.exit()
     cm = EncoderMeasure()
     print cm
+    #afiseaza ok citirea de pe encoder pana aici
     #speed is a vlue between 0 and 99 that's what PWM library expects
     #set("duty", str(speed))
 
