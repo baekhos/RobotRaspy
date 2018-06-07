eb914b810d4276c93f29e3f96ceb703941177fb2
baekhos <george.nitrogen@gmail.com>
2018-06-07 16:41:39 +0300

    Add files via upload

--- sub_motors.py
+++ sub_motors.py
@@ -15,40 +15,109 @@ import sys, math, tf
 #pe 20 de roatatii avem 20 de cm
 #deci 1 cm pe gaura encoder
 #rezolutia encoder ului este de ~0.5 cm
+GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 GPIO.setup(40,GPIO.IN)
+GPIO.setup(32,GPIO.IN)
 
 class Motor():
     def __init__(self):        
         sub = rospy.Subscriber('direction', String, self.callback)
-        self.direction="X"
-        self.oldValue=self.newValue=GPIO.input(40) 
+        
+         
         #setup pwm
         self.fs=GPIO.PWM(11,50) #spate dreapta- ok
         self.fd=GPIO.PWM(15,50) #fata dreapta-ok
         self.ss=GPIO.PWM(7,50) #spate stanga-ok
         self.sd=GPIO.PWM(13,50) #fata dreapta-ok
-        self.speed=30
-        self.turnspeed=16
+        #-----------------------------------
+        self.directive='X'
+        #vom declara 2 viteze ale rotilor
+        #Viteza rotii stangi si viteza rotii drepte 
+        self.vs=self.vd=0
+        #--------------------------------------------
+        #de asemenea vom avea nevoie de numarul de rotatii pe fiecare roata
+        #si de citirea anterioara a encoder ului pe fiecare roata
+        self.readS=GPIO.input(40)
+        self.readD=GPIO.input(32)
+        self.distS=0
+        self.distD=0
+        self.toggle=True
+        #--------------------------------
+        self.speed=12
+        self.turnspeed=10
+        self.limit =25
+        self.nt=time.time()
         self.pub_odom = rospy.Publisher('odom', Odometry, queue_size=10)
         self.bc_odom = tf.TransformBroadcaster()
 
         #Coordonate carteziene
         self.x, self.y, self.th = 0.0, 0.0, 0.0
         self.vx, self.vth = 0.0, 0.0
-
+        self.actual=False
         self.cur_time = rospy.Time.now()
         self.last_time = self.cur_time
+
+    def distanceS(self):
+        read=GPIO.input(40)
+        #print ("encoder stanga : %d" ,read )
+        if read==self.readS:
+            return 0
+        else: 
+            self.readS=read
+            self.actual=True
+            return 1    
+    
+    def distanceD(self):
+        read=GPIO.input(32)
+        #print ("encoder dreapta : %d" ,read )        
+        if read==self.readD:
+            return 0
+        else:
+            self.readD=read           
+            return 1    
+    
+
+    def speedCalibration(self):
+        self.distD+=self.distanceD()  
+        self.distS+=self.distanceS()              
+        t=time.time()
+        dt=t-self.nt
+        if  dt>0.1:            
+            speedS=self.distS/dt
+            speedD=self.distD/dt            
+            print ("viteza pe roata dreapta este: %d iar pwm-ul e de %d iar pe roata stanga este: %d iar pwm-ul e de %d " % ( speedD,self.vd,speedS,self.vs))
+            delta=speedS-speedD
+            if self.vd < self.limit and self.vs < self.limit:
+                if delta> 1.5:
+                    self.vd+=self.vd*(delta/speedD)
+		    if self.vd>self.limit:
+			self.vd=self.limit
+                elif delta < -1.5:
+                    delta=abs(delta)
+                    self.vs+=self.vs*(delta/speedS)
+		    if self.vs>self.limit:
+			self.vs=self.limit
+            else:                
+                if delta> 1.5:
+                    self.vs-=self.vs*(delta/speedS)                    
+                elif delta < -1.5:
+                    delta=abs(delta)                
+                    self.vd-=self.vd*(delta/speedD)
+            self.distS=self.distD=0.001
+            self.toggle= not self.toggle
+            self.nt=time.time()
+
 
     def send_odom(self):
         self.cur_time = rospy.Time.now()
 
-        self.EncoderMeasure(self.direction)    
-        #dt = self.cur_time.to_sec() - self.last_time.to_sec()
+        self.CurrentPosition()   
+        
         self.x += self.vx * math.cos(self.th) #* dt
         self.y += self.vx * math.sin(self.th) #* dt
         self.th += self.vth #* dt 
@@ -69,76 +138,92 @@ class Motor():
         odom.twist.twist.angular.z = self.vth
 
         self.pub_odom.publish(odom)
+        self.motorFunction()
+        if self.directive != 'X':            
+            self.speedCalibration()
 
         self.last_time = self.cur_time
 
     def forward(self):    
         self.ss.stop()
         self.sd.stop()
-        self.fs.start(self.speed)
-        self.fd.start(self.speed)
-        print "move forward"
+        self.fs.start(self.vs)
+        self.fd.start(self.vd)
+        #print "move forward"
 
     def backwards(self):
-        self.ss.start(self.speed)
-        self.sd.start(self.speed)
+        self.ss.start(self.vs)
+        self.sd.start(self.vd)
         self.fs.stop()
         self.fd.stop()
-        print "move backwards"
+        #print "move backwards"
+        
     def turnRight(self):
         self.ss.stop()
-        self.sd.start(self.turnspeed)
-        self.fs.start(self.turnspeed)
+        self.sd.start(self.vd)
+        self.fs.start(self.vs)
         self.fd.stop()
-        print "turn right"
+        #print "turn right"
+        
     def turnLeft(self):    
-        self.ss.start(self.turnspeed)
+        self.ss.start(self.vs)
         self.sd.stop()
         self.fs.stop()
-        self.fd.start(self.turnspeed)
-        print "turn left"
+        self.fd.start(self.vd)
+        #print "turn left"
+        
     def stop(self):
         self.ss.stop()
         self.sd.stop()
         self.fs.stop()
         self.fd.stop()
-        print "stop"
+           
 
-    def EncoderMeasure(self,direction):
-        cm=0
-        self.newValue = GPIO.input(40)
-        if self.newValue!=self.oldValue :
-            cm+=0.5
-        self.oldValue=self.newValue
-        if cm!=0:             
-            if direction=="W":
-                self.vx+= cm*0.001
-            elif direction == "S":
-                self.vx-=cm*0.001
-            elif direction == "D":
-                self.vth-=3.14*(cm/43.5)
-            elif direction == "A":
-                self.vth+=3.14*(cm/38)
-            
+    def CurrentPosition(self):
+        # step=False
+        # newValue = GPIO.input(40)
+        # if newValue!=self.oldValue :
+        #     step=True
+        # self.oldValue=newValue
+        if self.actual == True:             
+            if self.directive=="W":
+                self.vx+= 0.005
+            elif self.directive == "S":
+                self.vx-= 0.005
+            elif self.directive == "D":
+                self.vth-=0.0357
+            elif self.directive == "A":
+                self.vth+=0.0412
+        self.actual = False   
     #43.5 si 38 cm au fost calculate apriori si reprezinta distanta citita
     #de encoder pentru o rotatie de 360 de grade pe roata din dreapta
 
-    def callback(self,msg):
-        self.direction=msg.data
-        if msg.data=="W":
+    def motorFunction(self):        
+        if self.directive=="W":
             self.forward()
-        elif msg.data == "S":
+        elif self.directive == "S":
             self.backwards()
-        elif msg.data == "D":
+        elif self.directive == "D":
             self.turnRight()
-        elif msg.data == "A":
+        elif self.directive == "A":
             self.turnLeft()
-        elif msg.data == "X":
+        elif self.directive == "X":
             self.stop()
-        elif msg.data == "Q":
+        elif self.directive == "Q":
             GPIO.cleanup()
             ros.shutdown()
-        
+
+    def callback(self,msg):        
+        self.directive=msg.data
+        if self.directive =="W" or self.directive=="S":
+            self.vs=14
+	    self.vd=17
+            self.limit =25
+        elif self.directive =="A" or self.directive =="D":
+            self.vs=self.vd=12
+            self.limit =18
+        self.nt=time.time()
+        distD=distS=0
 
   
 
@@ -146,7 +231,7 @@ if __name__ == '__main__':
     rospy.init_node('Motor_Subscriber')
     m = Motor()
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(250)
     while not rospy.is_shutdown():
         m.send_odom()
         rate.sleep()
